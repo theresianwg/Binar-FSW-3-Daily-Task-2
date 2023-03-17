3fa372c12e0958039c6e5be4a6671553b0952fa9
theresianwg <theresianawangsih@gmail.com>
2023-03-17 17:53:16 +0700

    dt-3 masih ga bisa

---
+++ latihan3.js
@@ -0,0 +1,454 @@
+const data = [
+    {
+        "_id": "6410ad9ed00fb574456d910e",
+        "age": 35,
+        "eyeColor": "brown",
+        "name": "Stewart Clark",
+        "address": "Bali",
+        "registered": "2022-12-09T12:14:16 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "basketball"
+              },
+              {
+                "id": 1,
+                "hobby": "football"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Shifly",
+            "isActive": false,
+            "favoriteFruit": "strawberry",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Theresia",
+            "isActive": true,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "football"
+              },
+              {
+                "id": 1,
+                "hobby": "football"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9e73c1ea160668b08f",
+        "age": 38,
+        "eyeColor": "brown",
+        "name": "Charmaine Mcbride",
+        "address": "Bali",
+        "registered": "2022-08-20T06:52:16 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Theresia",
+            "isActive": false,
+            "favoriteFruit": "banana",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "football"
+              },
+              {
+                "id": 2,
+                "hobby": "football"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "banana",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "baseball"
+              },
+              {
+                "id": 1,
+                "hobby": "football"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Farid",
+            "isActive": true,
+            "favoriteFruit": "apple",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "baseball"
+              },
+              {
+                "id": 1,
+                "hobby": "football"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9e7a5fcc3db5ca4fea",
+        "age": 31,
+        "eyeColor": "blue",
+        "name": "Lucile Collins",
+        "address": "Jakarta",
+        "registered": "2021-03-26T06:38:12 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "strawberry",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "football"
+              },
+              {
+                "id": 2,
+                "hobby": "football"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Theresia",
+            "isActive": true,
+            "favoriteFruit": "strawberry",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "basketball"
+              },
+              {
+                "id": 2,
+                "hobby": "baseball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "strawberry",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "football"
+              },
+              {
+                "id": 2,
+                "hobby": "baseball"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9ee8a6ec44659f535e",
+        "age": 31,
+        "eyeColor": "green",
+        "name": "Owens Ellison",
+        "address": "Bandung",
+        "registered": "2022-03-08T05:05:41 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Farid",
+            "isActive": true,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "football"
+              },
+              {
+                "id": 2,
+                "hobby": "baseball"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "strawberry",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "football"
+              },
+              {
+                "id": 1,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Theresia",
+            "isActive": true,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "football"
+              },
+              {
+                "id": 1,
+                "hobby": "basketball"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9e3d5f242092ac1c36",
+        "age": 31,
+        "eyeColor": "green",
+        "name": "Kristin Glass",
+        "address": "Bandung",
+        "registered": "2020-01-16T02:48:53 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Shifly",
+            "isActive": true,
+            "favoriteFruit": "strawberry",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "banana",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "basketball"
+              },
+              {
+                "id": 1,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Theresia",
+            "isActive": true,
+            "favoriteFruit": "banana",
+            "gender": "female",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9e07eeab103aa1f3e2",
+        "age": 27,
+        "eyeColor": "green",
+        "name": "Shields Bryant,Kristin Glass, Owens Ellison, Lucile Collins",
+        "address": "Bali",
+        "registered": "2022-10-18T03:26:28 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Farid",
+            "isActive": true,
+            "favoriteFruit": "strawberry",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "basketball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Farid",
+            "isActive": false,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "football"
+              },
+              {
+                "id": 2,
+                "hobby": "baseball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Shifly",
+            "isActive": false,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "football"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "_id": "6410ad9efa47c4aaad1f79fe",
+        "age": 32,
+        "eyeColor": "brown",
+        "name": "Munoz Reynolds",
+        "address": "Bandung",
+        "registered": "2020-01-16T11:50:59 -07:00",
+        "friends": [
+          {
+            "id": 0,
+            "name": "Theresia",
+            "isActive": false,
+            "favoriteFruit": "strawberry",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          },
+          {
+            "id": 1,
+            "name": "Theresia",
+            "isActive": false,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 2,
+                "hobby": "basketball"
+              },
+              {
+                "id": 1,
+                "hobby": "baseball"
+              }
+            ]
+          },
+          {
+            "id": 2,
+            "name": "Shifly",
+            "isActive": false,
+            "favoriteFruit": "apple",
+            "gender": "male",
+            "hobby": [
+              {
+                "id": 1,
+                "hobby": "baseball"
+              },
+              {
+                "id": 2,
+                "hobby": "basketball"
+              }
+            ]
+          }
+        ]
+      }
+]
+
+
+function person(name, eyeColor, age, addres) {
+    // console.log(`hi, nama saya ${name}, Alamat saya berada di ${addres === 'bali'}`)
+}
+
+function chekcAddress(address) { 
+    return address === "bali" 
+} 
+ 
+person("Stewart Clark", "brown", 20, "bali")
+person("Charmaine Mcbride", "blue", 21, "surabaya")
+person("Kristin Glass","green", 10, "bali")
+person("Owens Ellison","brown", 21, "gresik")
+person("Lucile Collins", "blue", 10, "bandung")
+
+function loopingData(person) { 
+    let arr = [] 
+    for (let i = 0; i < person.length; i++) { 
+        if (chekcAddress(person[i].address)) { 
+            arr.push(person[i]) 
+            console.log(person[i].address) 
+        } 
+    }
+}
